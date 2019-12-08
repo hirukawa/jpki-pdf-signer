f44fd7968c14a5257b788a4435969dc289690194
hirukawa_ryo <hirukawa_ryo@f30e787d-f73b-42d4-b4a2-d790b30a04d0>
2019-12-08 06:38:39 +0000

    * jpki-pdf-signer 0.5 コードを全面的に書き直して刷新しました。
    git-svn-id: https://svn.osdn.net/svnroot/jpki/jpki-pdf-signer/trunk@33 f30e787d-f73b-42d4-b4a2-d790b30a04d0

--- src/main/java/net/osdn/jpki/ui/ListCellUpdateItemListener.java
+++
@@ -1,9 +0,0 @@
-package net.osdn.jpki.ui;
-
-import javafx.scene.control.ListCell;
-
-public interface ListCellUpdateItemListener<T> {
-	
-	public void updateItem(ListCell<T> cell, T item, boolean empty);
-
-}
